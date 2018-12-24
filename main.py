2ceed19d480c34430bd1aae17a0ca0b0b0084c20
gabrielaelisa <gabriela.mendoza@ug.uchile.cl>
2018-12-23 22:39:03 -0300

    code working

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from headers import *
 
 
-@np.vectorize
-def exponent(num):
-    try:
-        return math.exp(num)
-    except OverflowError:
-        return float('inf')
-
 iters= 0
 def P_X_skin(pixel):
     """
@@ -20,67 +13,25 @@ def P_X_skin(pixel):
 
     X= np.tile(pixel, (16, 1))
     X_u= X-mean_skin
-    det= np.prod(cov_skin_1,axis=1)
-    #det= np.linalg.det(cov_skin)
+    det= np.prod(cov_skin,axis=1)
     A=  w_i_skin*(1/(math.pow(math.pi*2, 3/2)*det))
-    cov_inv= np.linalg.inv(cov_skin)
-    #--------------------------------------------------
-    x_u_0 = (X_u * X_u)[:, [0]] / cov_skin_1[:, [0]]
-    x_u_1 = (X_u * X_u)[:, [1]] / cov_skin_1[:, [1]]
-    x_u_2 = (X_u * X_u)[:, [2]] / cov_skin_1[:, [2]]
+    B = (X_u *X_u/cov_skin)
+    C = np.exp((-1 / 2) *B)
+    return np.sum(np.dot(A,C))
 
-    B = x_u_0 + x_u_1 + x_u_2
-    try:
-        C = np.vectorize(math.exp)((-1 / 2) * B)
-    except OverflowError:
-        C= float('inf')
-    return np.sum(A*C)
 
 def P_X_no_skin(pixel):
     global iters
     iters += 1
     print(iters)
+
     X = np.tile(pixel, (16, 1))
     X_u = X - mean_non_skin
-    det = np.prod(cov_non_skin_1, axis=1)
+    det = np.prod(cov_non_skin, axis=1)
     A = w_i_non_skin * (1 / (math.pow(math.pi * 2, 3 / 2) * det))
-    cov_inv = np.linalg.inv(cov_non_skin)
-    #------------------------------------------------
-
-    x_u_0=(X_u*X_u)[:,[0]]/cov_non_skin_1[:,[0]]
-    x_u_1 = (X_u * X_u)[:, [1]]/cov_non_skin_1[:,[1]]
-    x_u_2 = (X_u * X_u)[:, [2]]/cov_non_skin_1[:,[2]]
-
-    B = x_u_0+x_u_1+x_u_2
-    try:
-        C = np.vectorize(math.exp)((-1 / 2) * B)
-    except OverflowError:
-        C = float('inf')
-    return np.sum(A * C)
-
-
-"""
-def P_X_no_skin(pixel):
-    global iters
-    iters+=1
-    print(iters)
-    X= np.tile(pixel, (16, 1))
-    X_u= X-mean_non_skin
-    det= np.linalg.det(cov_non_skin)
-    A=  w_i_non_skin*(1/(math.pow(math.pi*2, 3/2)*det))
-    #cov_inv= np.linalg.inv(cov_non_skin)
-    B= np.dot(X_u * cov_non_skin_1, X_u.transpose())
-    C= np.vectorize(exponent)((-1 / 2) * B)
-
-    
-    return np.sum(A*C)
- 
-    try:
-        C = np.vectorize(math.exp)((-1 / 2) * B)
-    except OverflowError:
-        C= float('inf')
-    return np.sum(A*C)
-"""
+    B = (X_u * X_u / cov_non_skin)
+    C = np.exp((-1 / 2) * B)
+    return np.sum(np.dot(A, C))
 
 
 def classify(image, phi):
@@ -94,11 +45,11 @@ if __name__ == '__main__':
     image2= image.copy()
 
     P_skin=np.apply_along_axis(P_X_skin, 2, image)
-    #P_non_skin= np.apply_along_axis(P_X_no_skin, 2, image2)
-    #result= P_skin/P_non_skin
-    #print(result)
+    P_non_skin= np.apply_along_axis(P_X_no_skin, 2, image2)
+    result= P_skin/P_non_skin
+    print(result)
     print(P_skin)
-    skin=classify(P_skin, float(phi))
+    skin=classify(result, float(phi))
     imgplot = plt.imshow(skin, cmap='gray')
     plt.show()
 
