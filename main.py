47df6574c0ae76a6bdea5a1185d8715858be6fda
gabrielaelisa <gabriela.mendoza@ug.uchile.cl>
2018-12-23 21:21:03 -0300

    changes

--- main.py
+++ main.py
@@ -24,7 +24,12 @@ def P_X_skin(pixel):
     #det= np.linalg.det(cov_skin)
     A=  w_i_skin*(1/(math.pow(math.pi*2, 3/2)*det))
     cov_inv= np.linalg.inv(cov_skin)
-    B = np.dot(X_u * np.diagonal(cov_inv, 0, 2, 1), X_u.transpose())
+    #--------------------------------------------------
+    x_u_0 = (X_u * X_u)[:, [0]] / cov_skin_1[:, [0]]
+    x_u_1 = (X_u * X_u)[:, [1]] / cov_skin_1[:, [1]]
+    x_u_2 = (X_u * X_u)[:, [2]] / cov_skin_1[:, [2]]
+
+    B = x_u_0 + x_u_1 + x_u_2
     try:
         C = np.vectorize(math.exp)((-1 / 2) * B)
     except OverflowError:
@@ -40,7 +45,13 @@ def P_X_no_skin(pixel):
     det = np.prod(cov_non_skin_1, axis=1)
     A = w_i_non_skin * (1 / (math.pow(math.pi * 2, 3 / 2) * det))
     cov_inv = np.linalg.inv(cov_non_skin)
-    B = np.dot(X_u * np.diagonal(cov_inv, 0, 2, 1), X_u.transpose())
+    #------------------------------------------------
+
+    x_u_0=(X_u*X_u)[:,[0]]/cov_non_skin_1[:,[0]]
+    x_u_1 = (X_u * X_u)[:, [1]]/cov_non_skin_1[:,[1]]
+    x_u_2 = (X_u * X_u)[:, [2]]/cov_non_skin_1[:,[2]]
+
+    B = x_u_0+x_u_1+x_u_2
     try:
         C = np.vectorize(math.exp)((-1 / 2) * B)
     except OverflowError:
@@ -83,12 +94,11 @@ if __name__ == '__main__':
     image2= image.copy()
 
     P_skin=np.apply_along_axis(P_X_skin, 2, image)
-    P_non_skin= np.apply_along_axis(P_X_no_skin, 2, image2)
-    result= P_skin/P_non_skin
+    #P_non_skin= np.apply_along_axis(P_X_no_skin, 2, image2)
+    #result= P_skin/P_non_skin
+    #print(result)
     print(P_skin)
-    print(P_non_skin)
-    print(result)
-    skin=classify(result, float(phi))
+    skin=classify(P_skin, float(phi))
     imgplot = plt.imshow(skin, cmap='gray')
     plt.show()
 
