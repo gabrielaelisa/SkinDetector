2d6a984034e8afc1b94accb6761b42b08c88f771
gabrielaelisa <gabriela.mendoza@ug.uchile.cl>
2018-12-28 17:14:19 -0300

    commit before refactor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from headers import *
 
+iters = 0
 
-iters= 0
+
 def P_X_skin(pixel):
     """
     :param pixel: color pixel
@@ -9,21 +10,21 @@ def P_X_skin(pixel):
 
     global iters
     iters += 1
-    print(iters)
+    #print(iters)
 
-    X= np.tile(pixel, (16, 1))
-    X_u= X-mean_skin
-    det= np.prod(cov_skin,axis=1)
-    A=  w_i_skin*(1/(math.pow(math.pi*2, 3/2)*det))
-    B = (X_u *X_u/cov_skin)
-    C = np.exp((-1 / 2) *B)
-    return np.sum(np.dot(A,C))
+    X = np.tile(pixel, (16, 1))
+    X_u = X - mean_skin
+    det = np.prod(cov_skin, axis=1)
+    A = w_i_skin * (1 / (math.pow(math.pi * 2, 3 / 2) * det))
+    B = (X_u * X_u / cov_skin)
+    C = np.exp((-1 / 2) * B)
+    return np.sum(np.dot(A, C))
 
 
 def P_X_no_skin(pixel):
     global iters
     iters += 1
-    print(iters)
+    #print(iters)
 
     X = np.tile(pixel, (16, 1))
     X_u = X - mean_non_skin
@@ -35,25 +36,20 @@ def P_X_no_skin(pixel):
 
 
 def classify(image, phi):
-    return image> phi
+    return image > phi
+
 
 if __name__ == '__main__':
+    path_image = sys.argv[1]
+    phi = sys.argv[2]
+    print(float(phi))
+    image = io.imread(path_image) * 1.0
+    image2 = image.copy()
 
-    path_image= sys.argv[1]
-    phi= sys.argv[2]
-    image = io.imread(path_image)*1.0
-    image2= image.copy()
-
-    P_skin=np.apply_along_axis(P_X_skin, 2, image)
-    P_non_skin= np.apply_along_axis(P_X_no_skin, 2, image2)
-    result= P_skin/P_non_skin
-    print(result)
-    print(P_skin)
-    skin=classify(result, float(phi))
+    P_skin = np.apply_along_axis(P_X_skin, 2, image)
+    P_non_skin = np.apply_along_axis(P_X_no_skin, 2, image2)
+    result = P_skin / P_non_skin
+    skin = classify(result, float(phi))
     imgplot = plt.imshow(skin, cmap='gray')
+    plt.axis('off')
     plt.show()
-
-
-
-
-
